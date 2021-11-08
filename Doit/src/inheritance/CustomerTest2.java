f98290b80f55eb8c0f4a6a9ced20a30157887de7
dani820 <danipark.st@gmail.com>
2021-11-08 22:30:48 +0900

    1108_2230 Learning DoIt JAVA ~ chapter 8 / test & customer sample

--- Doit/src/inheritance/CustomerTest2.java
+++ Doit/src/inheritance/CustomerTest2.java
@@ -2,11 +2,13 @@ package inheritance;
 
 public class CustomerTest2 {
 	public static void main(String[] args) {
-		VIPCustomer vipKim = new VIPCustomer();
-		vipKim.setCustomerName("김유신");
-		vipKim.setCustomerID(10020);
-		vipKim.bonusPoint = 10000;
-		System.out.println(vipKim.showCustomerInfo());
+//		VIPCustomer vipKim = new VIPCustomer(10020, "김유신", 42);
+		Customer vc = new VIPCustomer(10020, "김유신", 42);
+//		vipKim.setCustomerName("김유신");
+//		vipKim.setCustomerID(10020);
+//		vipKim.bonusPoint = 10000;
+//		System.out.println(vipKim.showCustomerInfo());
+//		System.out.println(vipKim.showVIPInfo());
 		
 	}
 }
--- Doit/src/inheritance/VIPCustomer.java
+++ Doit/src/inheritance/VIPCustomer.java
@@ -12,13 +12,27 @@ public class VIPCustomer extends Customer {
 		System.out.println("VIPCustomer() 생성자 호출");
 	}
 	
-//	public int calcPrice(int price) {
-//		bonusPoint += price * bonusRatio;
-//		return price - (int)(price * saleRatio);
-//	}
+	public VIPCustomer(int customerID, String customerName, int agentID) {
+		super(customerID, customerName);
+		customerGrade = "GOLD"; // Customer 클래스에서 해당 변수의 접근제어자가 private 이기 때문에 오류 발생. 외부 클래스에서 사용 불가.
+		bonusRatio = 0.05;
+		saleRatio = 0.1;
+		this.agentID = agentID;
+//		System.out.println("VIPCustomer(int, String) 생성자 호출");
+	}
+	
+	@Override
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		return price - (int)(price * saleRatio);
+	}
 	
 	public int getAgentID() {
 		return agentID;
+	}
+
+	public String showVIPInfo() {
+		return super.showCustomerInfo() + " 담당 상담원 아이디는 " + agentID + "입니다.";
 	}
 	
 //	public String showCustomerInfo() {
