350f67d8d10216259a7bf9a03cf45ac551283fcd
dani820 <danipark.st@gmail.com>
2021-11-26 14:25:53 +0900

    1126 1425 polymorphism re

--- Doit/src/polymorphism/AnimalTest.java
+++ Doit/src/polymorphism/AnimalTest.java
@@ -1,16 +1,16 @@
-package AnimalTest;
+package polymorphism;
 
 import java.util.ArrayList;
 
 // 상위 클래스
-class Animal {
+class Animal1 {
 	public void move() {
 		System.out.println("동물이 움직입니다.");
 	}
 }
 
-// 상위 클래스 Animal 을 상속받은 하위 클래스 Human
-class Human extends Animal {
+// 상위 클래스 Animal1 을 상속받은 하위 클래스 Human1
+class Human1 extends Animal1 {
 	public void move() {
 		System.out.println("사람이 두 발로 걷습니다.");
 	}
@@ -20,8 +20,8 @@ class Human extends Animal {
 	}
 }
 
-//상위 클래스 Animal 을 상속받은 하위 클래스 Tiger
-class Tiger extends Animal {
+//상위 클래스 Animal1 을 상속받은 하위 클래스 Tiger1
+class Tiger1 extends Animal1 {
 	public void move() {
 		System.out.println("호랑이가 네 발로 뜁니다.");
 	}
@@ -31,8 +31,8 @@ class Tiger extends Animal {
 	}
 }
 
-//상위 클래스 Animal 을 상속받은 하위 클래스 Eagle
-class Eagle extends Animal {
+//상위 클래스 Animal1 을 상속받은 하위 클래스 Eagle1
+class Eagle1 extends Animal1 {
 	public void move() {
 		System.out.println("독수리가 하늘을 납니다.");
 	}
@@ -45,7 +45,7 @@ class Eagle extends Animal {
 public class AnimalTest {
 	// 배열 자료형 Animal 로 animal List 생성
 	// 전역변수이자 멤버변수 aniList
-	ArrayList<Animal> aniList = new ArrayList<Animal>();
+	ArrayList<Animal1> aniList = new ArrayList<Animal1>();
 	
 	public static void main(String[] args) {
 		AnimalTest aTest = new AnimalTest();
@@ -55,17 +55,17 @@ public class AnimalTest {
 	}
 	
 	public void addAnimal() {
-		// 각각 Human, Tiger, Eagle 인스턴스가 Human 형으로 묵시적 형 변환 되어
+		// 각각 Human1, Tiger1, Eagle1 인스턴스가 Human1 형으로 묵시적 형 변환 되어
 		// 리스트에 담긴다.
-		aniList.add(new Human());
-		aniList.add(new Tiger());
-		aniList.add(new Eagle());
+		aniList.add(new Human1());
+		aniList.add(new Tiger1());
+		aniList.add(new Eagle1());
 		
 		// 상속 받은 하위클래스에서 메소드가 재정의되었기 때문에
 		// 각각 알맞은 하위 클래스 메소드가 출력된다.
 		// 배열 요소가 Animal 형이기 때문에
 		// 다른 동물 클래스들에서 제공하는 readBook(), hunting() 등의 메소드는 사용할 수 없다.
-		for(Animal ani : aniList) {
+		for(Animal1 ani : aniList) {
 			ani.move();
 		}
 	}
@@ -76,15 +76,15 @@ public class AnimalTest {
 		// 다운캐스팅 위해 인스턴스 자료형을 비교하여
 		// 각각 해당 결과를 출력한다.
 		for(int i = 0; i < aniList.size(); i++) {
-			Animal ani = aniList.get(i);
-			if(ani instanceof Human) {
-				Human h = (Human)ani;
+			Animal1 ani = aniList.get(i);
+			if(ani instanceof Human1) {
+				Human1 h = (Human1)ani;
 				h.readBook();
-			}else if(ani instanceof Tiger) {
-				Tiger t = (Tiger)ani;
+			}else if(ani instanceof Tiger1) {
+				Tiger1 t = (Tiger1)ani;
 				t.hunting();
-			}else if(ani instanceof Eagle) {
-				Eagle e = (Eagle)ani;
+			}else if(ani instanceof Eagle1) {
+				Eagle1 e = (Eagle1)ani;
 				e.flying();
 			}else {
 				System.out.println("지원되지 않는 형입니다.");
