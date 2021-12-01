b3e49179b8f1039d51dc3a97c1a89feb989d10f0
dani820 <danipark.st@gmail.com>
2021-12-01 10:05:20 +0900

    1201 1005 extends interface

--- Doit/src/interfaceex/MyClass.java
+++ Doit/src/interfaceex/MyClass.java
@@ -2,17 +2,17 @@ package interfaceex;
 
 public class MyClass implements MyInterface {
 
-	@Override
+	@Override // X 인터페이스로부터 상속 받아 구현
 	public void x() {
 		System.out.println("x()");
 	}
 
-	@Override
+	@Override // Y 인터페이스로부터 상속 받아 구현
 	public void y() {
 		System.out.println("y()");
 	}
 
-	@Override
+	@Override // MyClass 인터페이스 구현
 	public void myMethod() {
 		System.out.println("myMethod()");
 	}
--- Doit/src/interfaceex/MyClassTest.java
+++ Doit/src/interfaceex/MyClassTest.java
@@ -4,12 +4,18 @@ public class MyClassTest {
 	public static void main(String[] args) {
 		MyClass mClass = new MyClass();
 		
+		// 상위 클래스인 X 형으로 대입하면
+		// X 클래스에 존재하는 메소드만 호출 가능
 		X xClass = mClass;
 		xClass.x();
 		
+		// 상위 클래스인 Y 형으로 대입하면
+		// Y 클래스에 존재하는 메소드만 호출 가능
 		Y yClass = mClass;
 		yClass.y();
 		
+		// 구현한 인터페이스형으로 대입하면
+		// 인터페이스가 상속한 모든 메소드 호출 가능
 		MyInterface iClass = mClass;
 		iClass.myMethod();
 		iClass.x();
