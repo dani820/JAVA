44e2624aaff5e764c5dac838944a52176dd24375
dani820 <danipark.st@gmail.com>
2021-12-12 17:00:44 +0900

    1212 1700 Class class

---
+++ Doit/src/classex/ClassTest.java
@@ -0,0 +1,16 @@
+package classex;
+
+public class ClassTest {
+	// forName() 메소드에서 발생하는 예외를 처리. 이름과 일치하는 클래스 없을 경우 ClassNotFoundException 발생
+	public static void main(String[] args) throws ClassNotFoundException {
+		Person person = new Person();
+		Class pClass1 = person.getClass(); // object 클래스의 getClass() 메소드 사용
+		System.out.println(pClass1.getName());
+		
+		Class pClass2 = Person.class; // class 파일 이름을 Class 변수에 직접 대입
+		System.out.println(pClass2.getName());
+		
+		Class pClass3 = Class.forName("classex.Person"); // Class.forName("클래스 이름") 메소드 사용 (!예외처리 필수)
+		System.out.println(pClass3.getName());
+	}
+}
