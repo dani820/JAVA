564c38347128e59c637831b25c74eff81c5355f4
dani820 <danipark.st@gmail.com>
2021-12-18 23:21:02 +0900

    1218 2320 Iterator

--- Doit/src/collection/arrayList/MemberArrayList.java
+++ Doit/src/collection/arrayList/MemberArrayList.java
@@ -1,6 +1,7 @@
 package collection.arrayList;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import collection.Member;
 
@@ -23,15 +24,28 @@ public class MemberArrayList {
 	}
 	
 	public boolean removeMember(int memberId) {
-		for(int i = 0; i < arrayList.size(); i++) { // for 문을 돌려서 리스트 멤버 존재 여부 확인
-			Member member = arrayList.get(i); // 각 인덱스에 해당하는 값을 member 객체에 담는다.
-			int tempId = member.getMemberId(); // member 객체의 memberId 값을 tempId 에 담아 비교
-			if(tempId == memberId) { // id가 매개변수와 일치하면
-				arrayList.remove(i); // 해당 회원 삭제
+//		for(int i = 0; i < arrayList.size(); i++) { // for 문을 돌려서 리스트 멤버 존재 여부 확인
+//			Member member = arrayList.get(i); // 각 인덱스에 해당하는 값을 member 객체에 담는다.
+//			int tempId = member.getMemberId(); // member 객체의 memberId 값을 tempId 에 담아 비교
+//			if(tempId == memberId) { // id가 매개변수와 일치하면
+//				arrayList.remove(i); // 해당 회원 삭제
+//				return true;
+//			}
+//		}
+//		System.out.println(memberId + "가 존재하지 않습니다."); // 해당 아이디를 못 찾은 경우
+//		return false;
+		
+		// Iterator<Member> 와 같이 제네릭 자료형으로 Iterator 가 순회할 요소의 자료형 지정
+		Iterator<Member> ir = arrayList.iterator(); // Iterator 반환 
+		while(ir.hasNext()) { // 요소가 있는 동안 다음 회원을 받환받는다.
+			Member member = ir.next();
+			int tempId = member.getMemberId();
+			if(tempId == memberId) {
+				arrayList.remove(member);
 				return true;
 			}
 		}
-		System.out.println(memberId + "가 존재하지 않습니다."); // 해당 아이디를 못 찾은 경우
+		System.out.println(memberId + "가 존재하지 않습니다.");
 		return false;
 	}
 	
