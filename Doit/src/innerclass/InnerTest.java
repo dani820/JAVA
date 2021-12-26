f69e95a89650ea554b6329542b50a39d6c949e35
dani820 <danipark.st@gmail.com>
2021-12-26 21:56:38 +0900

    1226 2156 static inner class

--- Doit/src/innerclass/InnerTest.java
+++ Doit/src/innerclass/InnerTest.java
@@ -39,6 +39,29 @@ class OutClass { // 외부 클래스
 	public void usingClass() {
 		inClass.inTest();
 	}
+	
+	
+	// 정적 내부 클래스 예제 추가 
+	static class InStaticClass { // 정적 내부 클래스의 일반 메소드
+		int inNum = 100; // 정적 내부 클래스의 인스턴스 변수 
+		static int sInNum = 200; // 정적 내부 클래스의 정적 변수
+		 
+		void inTest() { // 정적 내부 클래스의 일반 메소드 
+//			num += 10; // 외부 클래스의 인스턴스 변수는 사용할 수 없다.
+			System.out.println("InStaticClass inNum = " + inNum + "(내부 클래스의 인스턴스 변수 사용)");
+			System.out.println("InStaticClass sInNum = " + sInNum + "(내부 클래스의 정적 변수 사용)");
+			System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 정적 변수 사용)");
+		}
+		
+		static void sTest() { // 정적 내부 클래스의 정적 메소드
+//			num += 10; // 외부 클래스와 내부 클래스의 인스턴스 변수는 사용할 수 없다.
+//			inNum += 10;
+			System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 정적 변수 사용)");
+			System.out.println("InStaticClass sInNum = " + sInNum + "(내부 클래스의 정적 변수 사용)");
+			
+		}
+	}
+
 }
 
 public class InnerTest {
@@ -46,5 +69,15 @@ public class InnerTest {
 		OutClass outClass = new OutClass();
 		System.out.println("외부 클래스 이용하여 내부 클래스 기능 호출");
 		outClass.usingClass();
+		
+		System.out.println("//");
+		
+		// 외부 클래스 생성하지 않고 바로 정적 내부 클래스 생성 가능
+		OutClass.InStaticClass sInClass = new OutClass.InStaticClass();
+		System.out.println("정적 내부 클래스 일반 메소드 호출");
+		sInClass.inTest();
+		System.out.println();
+		System.out.println("정적 내부 클래스의 정적 메소드 호출");
+		OutClass.InStaticClass.sTest();
 	}
 }
