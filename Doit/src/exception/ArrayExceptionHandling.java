c00f8dd0b9d8d046c871300aceb11711e3b4e665
dani820 <danipark.st@gmail.com>
2021-12-29 20:33:30 +0900

    1229 2033 try-catch ArrayIndexOutOfBoundsException

--- Doit/src/exception/ArrayExceptionHandling.java
+++ Doit/src/exception/ArrayExceptionHandling.java
@@ -9,7 +9,7 @@ public class ArrayExceptionHandling {
 			// 예외 발생할 수 있으므로 try 블럭에 작성
 			for(int i = 0; i <= 5; i++) {
 				arr[i] = i;
-				System.out.println(arr[i]);
+				System.out.println(arr[i]); 
 			}
 		} catch(ArrayIndexOutOfBoundsException e) {
 			// 예외 발생 시 catch 블록 수행
